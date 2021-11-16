dacd903c7c15f249f56d449160e9650d825bc5c0
naumov <naumov99.rst@gmail.comgit config --global user.email naumov99.rst@gmail.comgit config --global user.email naumov99.rst@gmail.com>
2021-11-16 19:30:17 +0300

    Applied babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
-'use strict'
+'use strict';
 
-const path = require('path');
+let path = require('path');
 
 module.exports = {
-  mode: "development", 
-
-  entry: "./js/index.js", 
-
+  mode: 'production',
+  entry: './js/index.js',
   output: {
-		filename: "main.js", 
-		path: __dirname + '/dist/js'
-	},
+    filename: 'main.js',
+    path: __dirname + '/dist/js'
+  },
+  watch: true,
 
-	watch: true,
-    
-	devtool: 'source-map',
-	module: {}
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage" 
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
